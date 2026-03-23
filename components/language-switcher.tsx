fc820c1afe4d01100785fa18fd395e8084e78df8
ensite999-stack <ensite999@gmail.com>
2026-03-23 17:48:20 +0800

    Create language-switcher.tsx

---
+++ components/language-switcher.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { languageOptions, type LangCode } from '@/lib/i18n'
+import { useI18n } from '@/components/language-provider'
+
+export default function LanguageSwitcher() {
+  const { lang, setLang, t } = useI18n()
+
+  return (
+    <div className="lang-switcher-wrap">
+      <select
+        aria-label={t.common.language}
+        value={lang}
+        onChange={(e) => setLang(e.target.value as LangCode)}
+        className="lang-switcher"
+      >
+        {languageOptions.map((item) => (
+          <option key={item.code} value={item.code}>
+            {item.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
